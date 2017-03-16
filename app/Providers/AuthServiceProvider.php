9f556c10aa9e82519a14a8a54ff25c5a802ba446
junming4 <2284876299@qq.com>
2017-03-16 16:19:49 +0800

    首次提交

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        //
+        $this->registerPolicies($gate);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+## laravel 5.1 
+    1、创建权限提供者（1）、 php artisan  make:provider AuthServiceProvider
+                    （2）、配置conf/app.php   App\Providers\AuthServiceProvider::class,
+                    （3）、对  App\Providers\AuthServiceProvider.php 进行修改
+        
